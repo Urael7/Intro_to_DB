3230d412a7dd90f9a19468ccbc14b6b4afd3020a
Urael7 <uraelyafet@gmail.com>
2025-12-15 00:40:51 +0300

    Create alx_book_store database with exception handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 import mysql.connector
-from mysql.connector import Error
 
 try:
-    db = mysql.connector.connect(
+    connection = mysql.connector.connect(
         host="localhost",
         user="root",
         password=""
     )
 
-    cursor = db.cursor()
+    cursor = connection.cursor()
     cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print("Error while connecting to MySQL:", e)
 
 else:
     cursor.close()
-    db.close()
+    connection.close()
